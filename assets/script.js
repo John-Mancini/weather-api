d04627a404fe607fc681978843c813c99a6a53c4
John-Mancini <mancinijohn5988@gmail.com>
2021-04-06 12:24:35 -0400

    Added Variable calls and API_KEY to the page

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+var API_KEY = "32bd1aba40b578b2069db9c3cbbf0792";
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+// http://api.openweathermap.org/data/2.5/weather?q=Boston&appid=32bd1aba40b578b2069db9c3cbbf0792+
+let citySearch = $("#search-city");
+let citySearchBtn = $("#search-city-button");
+let temperature = $("#current-temp");
+let windSpeed = $("#current-wind-speed");
+let humidity = $("#current-humidity");
+let uvIndex = $("#uv-index");
+let cityName = $("#cityName");
+let weatherContent = $("#weather-content");
+let searchHistoryList = $("#search-history-list");
+let clearHistoryBtn = $("#clear-history");
+let fiveDayPanel = $("#five-day-forecast");
+
+let cityNames = [];
